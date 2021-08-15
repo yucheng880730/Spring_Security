9b6ed99c324b155acbdb2f0c2099f8d42e64d2d1
yucheng880730 <yucheng2k13@gmail.com>
2021-08-15 21:15:39 +0800

    preAuthorize

--- src/main/java/com/example/security_demo/student/StudentManagementController.java
+++ src/main/java/com/example/security_demo/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package com.example.security_demo.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -15,25 +16,31 @@ public class StudentManagementController {
             new Student(3,"Curry")
     );
 
+    // hasRole('ROLE_') hasAnyRole('ROLE') hasAuthority('permission') hasAnyAuthority('permission')
+
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents() {
         System.out.println("getAllStudents");
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student) {
         System.out.println("registerNewStudent");
         System.out.println(student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId) {
         System.out.println("deleteStudent");
         System.out.println(studentId);
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
         System.out.println("updateStudent");
         System.out.println(String.format("%s %s", studentId, student));
